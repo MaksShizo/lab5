b890d3ac0ef541501633efff238eb8d2b8ffb0d8
maksd <maksdi3@yandex.ru>
2024-04-07 19:40:46 +0500

    start (copy lab4)

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -1,20 +1,69 @@
-﻿// lab5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <mpi.h>
+#include <chrono>
 
-#include <iostream>
+const int N = 100;
+std::chrono::high_resolution_clock::time_point startTime, finishTime;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+double A[N][N];
+double B[N][N];
+double C[N][N];
+double D[N][N];
+
+void printMatrix(double A[N][N], int rows, int cols) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            std::cout << A[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+int main(int argc, char** argv) {
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    if (rank == 0) {
+        startTime = std::chrono::high_resolution_clock::now();
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < N; ++j) {
+                A[i][j] = i + 1;
+                B[i][j] = j + 1;
+                C[i][j] = 0;
+                D[i][j] = 0;
+            }
+        }
+    }
+
+    MPI_Bcast(A, N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(B, N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    int blockSize = N / size;
+    int remainder = N % size;
+    int start = rank * blockSize;
+    int end = start + blockSize;
+
+    if (rank == size - 1) {
+        end += remainder;
+    }
+
+    for (int i = start; i < end; ++i) {
+        for (int j = 0; j < N; ++j) {
+            for (int k = 0; k < N; ++k) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    MPI_Reduce(C, D, N * N, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        std::cout << "Resulting Matrix:" << std::endl;
+        printMatrix(D, N, N);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
